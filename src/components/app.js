89aff8d06a2ac16360ce754d760a042adb09d88e
Tushar Langer <tushar.langer@gmail.com>
2021-02-25 21:03:48 +0530

    initial setup

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <br />
+        <a
+          className="button-line"
+          href="https://github.com/Tushh007"
+          target="_blank"
+        >
+          Know more now
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
