fe7022fa507f2d982aded2a76f208dd5c1cb3ab8
Tushar Langer <tushar.langer@gmail.com>
2021-02-26 15:27:16 +0530

    cache busting: added content hash to filename

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     publicPath: '/',
   },
   module: {
