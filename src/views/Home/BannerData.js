62de9e918b3feec4388106d6f4f63981edef15b4
Ujwalcode24 <ujwalcode@gmail.com>
2021-03-01 19:41:24 +0530

    category tiles updated

---
+++ src/views/Home/BannerData.js
@@ -0,0 +1,19 @@
+import Banner1 from "../../assets/img/Banner/banner1.png";
+import Banner2 from "../../assets/img/Banner/banner2.png";
+import Banner3 from "../../assets/img/Banner/banner3.png";
+import Banner4 from "../../assets/img/Banner/banner4.png";
+
+export const bannerImages = [
+  {
+    img: Banner1,
+  },
+  {
+    img: Banner2,
+  },
+  {
+    img: Banner3,
+  },
+  {
+    img: Banner4,
+  },
+];
