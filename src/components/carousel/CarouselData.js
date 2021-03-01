99c316ba646639ee40e6b01b604a48872ef6e3be
Ujwalcode24 <ujwalcode@gmail.com>
2021-03-01 18:04:15 +0530

    navbar carousel

---
+++ src/components/carousel/CarouselData.js
@@ -0,0 +1,24 @@
+import bg1 from "../../assets/img/bg1.jpg";
+import bg2 from "../../assets/img/bg2.jpg";
+import bg3 from "../../assets/img/bg3.jpg";
+
+export const images = [
+  {
+    title: "Fashion Collection 2021",
+    subtitle: "Collection 2021",
+    buttonContent: "Learn More",
+    img: bg1,
+  },
+  {
+    title: "Fresh Collection 2021",
+    subtitle: "Collection 2021",
+    buttonContent: "Learn More",
+    img: bg2,
+  },
+  {
+    title: "Classic Collection 2021",
+    subtitle: "Collection 2021",
+    buttonContent: "Learn More",
+    img: bg3,
+  },
+];
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Search from "../Search/Search";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Badge from "@material-ui/core/Badge";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleDesktopProfileMenuclick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const menuId = "desktopProfileMenu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Login</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My Account</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My orders</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "mobileProfileMenu";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" >
+          <Badge badgeContent={0} color="secondary">
+            <ShoppingCartOutlinedIcon />
+          </Badge>
+        </IconButton>
+        <p>Cart</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications">
+          <Badge badgeContent={0} color="secondary">
+            <FavoriteBorderIcon />
+          </Badge>
+        </IconButton>
+        <p>Wishlist</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton aria-controls={menuId} aria-haspopup="true">
+          <PersonOutlineOutlinedIcon />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <nav className="navbar">
+      <h2 className="navbarLogo">Little Tags</h2>
+      <h2 className="navbarMobileLogo">LT</h2>
+      <div className="navbar_right">
+        <Search />
+        <div className="navbar_desktopMenu">
+          <IconButton
+            aria-controls={menuId}
+            aria-haspopup="true"
+            onClick={handleDesktopProfileMenuclick}
+          >
+            <PersonOutlineOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <FavoriteBorderIcon />
+          </IconButton>
+          <IconButton>
+            <ShoppingCartOutlinedIcon />
+          </IconButton>
+        </div>
+        <div className="navbar_mobileMenu">
+          <IconButton
+            aria-label="show more"
+            aria-controls={mobileMenuId}
+            aria-haspopup="true"
+            onClick={handleMobileMenuOpen}
+            color="inherit"
+          >
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      {renderMenu}
+      {renderMobileMenu}
+    </nav>
+  );
+};
+
+export default Navbar;
