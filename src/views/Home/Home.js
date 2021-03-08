890efdf264656caa69a1d7203b3e42a8431fc4c3
Tushar Langer <tushar.langer@gmail.com>
2021-03-08 10:20:20 +0530

    landing page major issues solved

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -13,19 +13,35 @@ const Home = () => {
         <div
           className="home__categories1"
           style={{ backgroundImage: `url(${bannerImages[0].img})` }}
-        ></div>
+        >
+          <div className="home__tileElement">
+            <button className="home__shopButton">Shop Now</button>
+          </div>
+        </div>
         <div
           className="home__categories2"
           style={{ backgroundImage: `url(${bannerImages[1].img})` }}
-        ></div>
+        >
+          <div className="home__tileElement">
+            <button className="home__shopButton">Shop Now</button>
+          </div>
+        </div>
         <div
           className="home__categories3"
           style={{ backgroundImage: `url(${bannerImages[2].img})` }}
-        ></div>
+        >
+          <div className="home__tileElement">
+            <button className="home__shopButton">Shop Now</button>
+          </div>
+        </div>
         <div
           className="home__categories4"
           style={{ backgroundImage: `url(${bannerImages[3].img})` }}
-        ></div>
+        >
+          <div className="home__tileElement">
+            <button className="home__shopButton">Shop Now</button>
+          </div>
+        </div>
       </div>
       <div className="products">
         <ProductRow title="Featured Products" items={PRODUCTS} />
