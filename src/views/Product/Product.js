6ad91f411bf48c2aeefa088afd860c3719c96b8f
Tushar Langer <tushar.langer@gmail.com>
2021-03-15 17:09:29 +0530

    new branch

--- src/views/Product/Product.js
+++ src/views/Product/Product.js
@@ -63,6 +63,7 @@ const Product = () => {
     const data = FAKEDATA[category.toUpperCase().replace(' ', '_')];
     const product = data.find((data) => data.id == productId);
     setProduct(product);
+    console.log(product, data);
   }, [category, product]);
 
   const handleChange = (event) => {
@@ -128,6 +129,7 @@ const Product = () => {
           </div>
           <div className="priceRating">
             <p className="price">₹ {product?.price}</p>
+            {console.log(product.rating)}
             <ProductRating rating={product?.rating} />
           </div>
           <div className="productInfo">
